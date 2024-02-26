bea41f93960fc7182d91dfc573829b24012c5354
Vieiravs2 <matheusemidio.canal@gmail.com>
2024-02-26 04:12:00 -0300

    Feat: Requisito 03 finalizado

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,30 @@ package com.betrybe.sistemadevotacao;
 /**
  * Classe Pessoa Eleitora.
  */
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  public PessoaEleitora(String nome, String cpf) {
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  /**
+   * Getter do CPF.
+   */
+  public String getCpf() {
+    return cpf;
+  }
+
+  /**
+   * Setter do CPF.
+   */
+  public void setCpf(String cpf) {
+    if (cpf.isEmpty()) {
+      throw new IllegalArgumentException("CPF Inválido.");
+    }
+
+    this.cpf = cpf;
+  }
 }
